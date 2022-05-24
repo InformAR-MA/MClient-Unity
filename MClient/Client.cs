f498737f60b570563693134d07dfbc74de1e44ed
MsZoezo <Z.Eijgenberger@gmail.com>
2022-05-24 19:51:05 +0200

    Basic test done

---
+++ MClient/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebSocketSharp;
+
+
+namespace MClient
+{
+    public class MClient
+    {
+        private WebSocket ws;
+
+        public MClient(string url, string id)
+        {
+            ws = new WebSocket(url);
+            ws.Connect();
+
+            ws.Send("{ \"type\": \"identifier\", \"id\": \"" + id + "\"}");
+        }
+    }
+}
