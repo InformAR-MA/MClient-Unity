cbdfe06506cc5b574d847a81a5b5beb2f1352eb9
MsZoezo <Z.Eijgenberger@gmail.com>
2022-05-25 12:03:24 +0200

    updates

---
+++ MClient-Console/Program.cs
@@ -0,0 +1,10 @@
+﻿using MClient;
+
+Client client = new Client("ws://localhost:8080", "Console");
+
+client.OnMessage += (sender, e) =>
+{
+    Console.WriteLine(e.sender + ": " + e.message);
+};
+
+while (true) {}
--- MClient/Client.cs
+++ MClient/Client.cs
@@ -3,20 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using WebSocketSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 
 namespace MClient
 {
-    public class MClient
+    public class Client
     {
         private WebSocket ws;
 
-        public MClient(string url, string id)
+        public class MessageArgs: EventArgs
         {
-            ws = new WebSocket(url);
-            ws.Connect();
+            public MessageArgs(string sender, string message)
+            {
+                this.sender = sender;
+                this.message = message;
+            }
 
-            ws.Send("{ \"type\": \"identifier\", \"id\": \"" + id + "\"}");
+            public string sender;
+            public string message;
+        }
+
+        public delegate void MessageHandler(object sender, MessageArgs e);
+        public event MessageHandler OnMessage;
+
+        public Client(string url, string id)
+        {
+            this.ws = new WebSocket(url);
+            this.ws.Connect();
+
+            this.ws.OnMessage += (sender, e) =>
+            {
+                JObject data = JObject.Parse(e.Data);
+
+                switch(data["type"].ToString())
+                {
+                    case "message":
+                        {
+                            MessageHandler handler = OnMessage;
+                            handler?.Invoke(this, new MessageArgs(data["sender"].ToString(), data["message"].ToString()));
+                            break;
+                        }
+
+                    default:
+                        {
+                            break;
+                        }
+                }
+            };
+
+            ws.OnClose += (sender, e) => {
+                Console.WriteLine("uhoh");
+            };
+            this.ws.Send("{ \"type\": \"identify\", \"id\": \"" + id + "\"}");
+        }
+
+        public void Send(string recipient, string message)
+        {
+            ws.Send("{\"recipient\": \"" + recipient + "\", \"message\": \"" + message + "\"}");
         }
     }
 }
